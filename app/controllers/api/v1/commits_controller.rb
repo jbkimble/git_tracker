3d379a99c825ae006b3a1dadabcfdf3d6fe4894e
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-14 09:49:47 -0700

    Parse response

--- app/controllers/api/v1/commits_controller.rb
+++ app/controllers/api/v1/commits_controller.rb
@@ -12,8 +12,9 @@ class Api::V1::CommitsController < ApplicationController
   private
     def commit_params
       # getting 500 error, added ['payload'] to line below to try to solve, havint confirmed it works
-      commit = params['payload']['head_commit']
-      { sha: commit['id'][0..6], message: commit['message'], author: commit['author']['name'], timestamp: commit['timestamp'] }
+      commit = JSON.parse(params['payload'])
+      
+      # { sha: commit['id'][0..6], message: commit['message'], author: commit['author']['name'], timestamp: commit['timestamp'] }
     end
 
 end

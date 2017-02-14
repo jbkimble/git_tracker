6b8df1d1886f5ce5cf88dabc0ade84face3a96a2
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-14 09:45:23 -0700

    Reapply create action logic

--- app/controllers/api/v1/commits_controller.rb
+++ app/controllers/api/v1/commits_controller.rb
@@ -2,12 +2,15 @@ class Api::V1::CommitsController < ApplicationController
   # protect_from_forgery with: :null_session
   skip_before_filter  :verify_authenticity_token
 
-  def create
 
+  def create
+    commit = Commit.new(commit_params)
+    commit.save
   end
 
   private
     def commit_params
+      # getting 500 error, added ['payload'] to line below to try to solve, havint confirmed it works
       commit = params['payload']['head_commit']
       { sha: commit['id'][0..6], message: commit['message'], author: commit['author']['name'], timestamp: commit['timestamp'] }
     end

40e44e45078a8119753204446badbd403eb33a76
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-14 09:54:43 -0700

    Use puts instead of render text

--- app/controllers/api/v1/commits_controller.rb
+++ app/controllers/api/v1/commits_controller.rb
@@ -6,14 +6,14 @@ class Api::V1::CommitsController < ApplicationController
   def create
     # commit = Commit.new(commit_params)
     # commit.save
-    render text: commit_params
+    puts commit_params
   end
 
   private
     def commit_params
       # getting 500 error, added ['payload'] to line below to try to solve, havint confirmed it works
       commit = JSON.parse(params['payload'])
-
+      JSON.parse(params.keys.first)
       # { sha: commit['id'][0..6], message: commit['message'], author: commit['author']['name'], timestamp: commit['timestamp'] }
     end
 

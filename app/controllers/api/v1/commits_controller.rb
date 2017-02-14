eeb55d58e39d7415ec87b3273a5912e8da2368f6
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-14 10:03:48 -0700

    Reinstate logic to commit_params and create action

--- app/controllers/api/v1/commits_controller.rb
+++ app/controllers/api/v1/commits_controller.rb
@@ -4,18 +4,15 @@ class Api::V1::CommitsController < ApplicationController
 
 
   def create
-    # commit = Commit.new(commit_params)
-    # commit.save
-    puts params
-    puts JSON.parse(params[:payload])
+    commit = Commit.new(commit_params)
+    puts commit
+    commit.save
   end
 
   private
     def commit_params
-      # getting 500 error, added ['payload'] to line below to try to solve, havint confirmed it works
-      # commit = JSON.parse(params['payload'])
-      # JSON.parse(params.keys.first)
-      # { sha: commit['id'][0..6], message: commit['message'], author: commit['author']['name'], timestamp: commit['timestamp'] }
+      commit = JSON.parse(params[:payload])['head_commit']
+      { sha: commit['id'][0..6], message: commit['message'], author: commit['author']['name'], timestamp: commit['timestamp'] }
     end
 
 end
